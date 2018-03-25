9f2c966a6a88145bde4a871404d578a1dd7608ea
Inhji <johnnie@posteo.de>
2018-03-25 13:26:20 +0200

    login works, add loggedIn check on layout, correct api url

--- components/layout.js
+++ components/layout.js
@@ -1,21 +1,59 @@
+import { Component } from 'react'
+import fetch from 'isomorphic-fetch'
 import Navbar from './navbar'
-import '../styles/styles.scss'
 import pkg from '../package.json'
+import '../styles/styles.scss'
 
-export default ({ children, title = 'Home' }) => (
-  <div>
-    <header>
-      <Navbar />
-    </header>
+export default class Layout extends Component {
+  state = {
+    loggedIn: null
+  }
 
-    <main id="site-content">
-      <div className="section">
-        <div className="container">{children}</div>
+  async componentWillMount () {
+    const res = await fetch('https://api.inhji.de/loggedin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include'
+    })
+
+    if (res.ok) {
+      const { id } = await res.json()
+
+      localStorage.setItem('CTHULHU_USER_ID', id)
+    } else {
+      localStorage.removeItem('CTHULHU_USER_ID')
+    }
+
+    this.setState({ loggedIn: res.ok })
+  }
+
+  render () {
+    return (
+      <div>
+        <header>
+          <Navbar />
+        </header>
+
+        <main id="site-content">
+          <div className="section">
+            <div className="container">
+              {this.state.loggedIn === null ? (
+                <div>loading</div>
+              ) : this.state.loggedIn === false ? (
+                <div>unauthorized</div>
+              ) : (
+                this.props.children
+              )}
+            </div>
+          </div>
+        </main>
+
+        <footer className="footer">
+          <div className="container">Cthulhu v{pkg.version}</div>
+        </footer>
       </div>
-    </main>
-
-    <footer className="footer">
-      <div className="container">Cthulhu v{pkg.version}</div>
-    </footer>
-  </div>
-)
+    )
+  }
+}
